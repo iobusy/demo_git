b111b56ae61128661bbcdb17ff7a79587a0402b0
BugHunt3r <jla92@free.fr>
2015-08-04 12:39:07 +0200

    Modification de la phrase à afficher

--- test2.php
+++ test2.php
@@ -6,7 +6,7 @@ $image = imagecreate(200,50);
 $orange = imagecolorallocate($image, 255, 128, 0);
 $blanc = imagecolorallocate($image, 255, 255, 255);
 
-imagestring($image, 4, 35, 15, "Salut Zéros !", $blanc);
+imagestring($image, 4, 35, 15, "Salut !", $blanc);
 imagepng($image);
 //header('Content-Type: image/png');
 //$im = imagecreatefrompng("images/logo.png");
