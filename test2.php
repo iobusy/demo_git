8aeba614c4ed0daaec6279cf29e4c5174539330b
BugHunt3r <jla92@free.fr>
2015-08-04 12:35:48 +0200

    Ajout d'un commentaire sur l'encodage

---
+++ test2.php
@@ -0,0 +1,17 @@
+<?php
+// Ne fonctionne pas si l'encodage est different de ANSI
+
+header ("Content-type: image/png");
+$image = imagecreate(200,50);
+$orange = imagecolorallocate($image, 255, 128, 0);
+$blanc = imagecolorallocate($image, 255, 255, 255);
+
+imagestring($image, 4, 35, 15, "Salut Zéros !", $blanc);
+imagepng($image);
+//header('Content-Type: image/png');
+//$im = imagecreatefrompng("images/logo.png");
+
+// imagepng($im,NULL,0,NULL);
+//imagepng($im);
+//imagedestroy($im);
+?>
